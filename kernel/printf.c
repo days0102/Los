3892b2100d958a9c31f1e3faebfc0b031bf8c308
days0102 <411883104@qq.com>
2022-08-13 19:13:04 +0800

    modify printf -num

--- kernel/printf.c
+++ kernel/printf.c
@@ -2,7 +2,7 @@
  * @Author: Outsider
  * @Date: 2022-07-11 10:42:08
  * @LastEditors: Outsider
- * @LastEditTime: 2022-08-13 18:10:56
+ * @LastEditTime: 2022-08-13 19:12:28
  * @Description: In User Settings Edit
  * @FilePath: /los/kernel/printf.c
  */
@@ -65,6 +65,11 @@ int printf(const char *fmt, ...)
 				{
 					// 获取参数，将指针指向下一个参数
 					int x = va_arg(vl, int);
+					if (x < 0)
+					{
+						outbuf[idx++] = '-';
+						x = -x;
+					}
 					int len = 0;
 					for (int n = x; n /= 10; len++)
 						;
